e7c1f940b4d427b7e58c4a56c92c3dd5d39db2d8
zeroheight <tokens-management@zeroheight.com>
2025-09-02 13:41:51 -0300

    Update Aurea - Foundations (Copy) from zeroheight

---
+++ teste/token_components_components.js
@@ -0,0 +1,73 @@
+module.exports = {
+  "color": {
+    "aurea": {
+      "input": {
+        "bg": {
+          "enabled": {
+            "value": "#f6f6f6",
+            "original": {
+              "value": "#f6f6f6"
+            },
+            "name": "ColorAureaInputBgEnabled",
+            "attributes": {
+              "category": "color",
+              "type": "aurea",
+              "item": "input",
+              "subitem": "bg",
+              "state": "enabled"
+            },
+            "path": [
+              "color",
+              "aurea",
+              "input",
+              "bg",
+              "enabled"
+            ]
+          },
+          "mouse-in": {
+            "value": "#f6f6f6",
+            "original": {
+              "value": "#f6f6f6"
+            },
+            "name": "ColorAureaInputBgMouseIn",
+            "attributes": {
+              "category": "color",
+              "type": "aurea",
+              "item": "input",
+              "subitem": "bg",
+              "state": "mouse-in"
+            },
+            "path": [
+              "color",
+              "aurea",
+              "input",
+              "bg",
+              "mouse-in"
+            ]
+          },
+          "target": {
+            "value": "#ffffff",
+            "original": {
+              "value": "#ffffff"
+            },
+            "name": "ColorAureaInputBgTarget",
+            "attributes": {
+              "category": "color",
+              "type": "aurea",
+              "item": "input",
+              "subitem": "bg",
+              "state": "target"
+            },
+            "path": [
+              "color",
+              "aurea",
+              "input",
+              "bg",
+              "target"
+            ]
+          }
+        }
+      }
+    }
+  }
+};
